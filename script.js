4694aa7170a1b093388b0600fe8a152e4332145e
Dark-Prince237 <rashidiqbal7141@gmail.com>
2021-11-16 08:45:01 +0530

    today done

--- script.js
+++ script.js
@@ -45,3 +45,42 @@ function silderfunc(num)
     
 }
 
+//slider end
+
+//countdown
+
+// Set the date we're counting down to
+var countDownDate = new Date("Jan 5, 2022 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="countdown"
+  document.getElementById("countdown").innerHTML =  hours + " : "
+  + minutes + " : " + seconds +" Left";
+  document.getElementById("countdown").style.fontSize="18px";
+  document.getElementById("countdown").style.color="grey";
+  document.getElementById("countdown").style.cursor="wait";
+
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
+
+//countdown ends
+
